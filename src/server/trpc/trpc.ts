262240dce77d7304b1e54a5f2f83ffeeca8d2dd7
zorro901 <zoro907@gmail.com>
2022-10-22 10:42:56 +0900

    [Frontend] trpc／client

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,12 +1,23 @@
 import { initTRPC, TRPCError } from "@trpc/server";
 import type { Context } from "./context";
 import superjson from "superjson";
+import { ZodError } from 'zod';
 
 export const t = initTRPC.context<Context>().create({
   transformer: superjson,
-  errorFormatter({ shape }) {
-    return shape;
-  },
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === 'BAD_REQUEST' &&
+          error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
+  }
 });
 
 export const authedProcedure = t.procedure.use(({ ctx, next }) => {
